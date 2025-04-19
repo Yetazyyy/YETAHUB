5221e9b11358c969ec38ec97310e69f29ea58c05
Yetazyyy <fecabookgaming@gmail.com>
2025-04-19 09:32:18 +0800

    Delete Rawrrr.lua

--- Rawrrr.lua
+++
@@ -1,6 +0,0 @@
-local char = game.Players.LocalPlayer.Character
-local client = require(char:WaitForChild("CharacterHandler"):WaitForChild("Client"))
-
-for k, v in pairs(client) do
-    print(k, v)
-end
